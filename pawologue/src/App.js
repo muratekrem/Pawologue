7b2fe21b7eaa3fa21e4611672ef2b6086f3ef840
muratekrem <110377394+muratekrem@users.noreply.github.com>
2024-03-08 20:02:16 +0300

    Maps

--- pawologue/src/App.js
+++ pawologue/src/App.js
@@ -1,10 +1,22 @@
-import './App.css';
+import React from "react";
+
 
 function App() {
   return (
     <div>
-      dsafsa
+      <div style={{display:"flex",justifyContent:"center"}}>
+        <button>
+          Sign in
+        </button>
+        <button>
+          Sign up
+        </button>
+      </div>
+      <div style={{display:"flex", alignContent:"center",justifyContent:"center"}}>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10312.425341935903!2d30.517380395490452!3d39.78674569721603!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14cc15e397ff02e5%3A0xc7c4c24413b5af1a!2sEspark%20Shopping%20Mall!5e0!3m2!1sen!2str!4v1709916751166!5m2!1sen!2str" width="600" height="450" allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade" title="Google Maps"></iframe>
     </div>
+    </div>
+    
   );
 }
 
