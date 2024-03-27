06d13f693b7e22635a7c9c66318b1b8a6878f494
muratekrem <110377394+muratekrem@users.noreply.github.com>
2024-03-27 17:46:14 +0300

    BlueMark

--- pawologue/src/Map/MapComponent.js
+++ pawologue/src/Map/MapComponent.js
@@ -1,14 +1,20 @@
 import React, { useState, useEffect } from "react";
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
-import { Loader } from '@googlemaps/js-api-loader';
+import { GoogleMap, LoadScript } from "@react-google-maps/api";
+import { Loader } from "@googlemaps/js-api-loader";
+import BlueMark from "../blueMark.jpg";
 
 const API_KEY = "AIzaSyDn_2p8NJHqWX_vHzv6MKyV7786XwUGtzo";
 
 const defaultCenter = {
   lat: 39.7896,
-  lng: 30.5162
+  lng: 30.5162,
 };
 const defaultZoom = 13;
+
+const vetsMarker = [
+  { name: "Vet1", location: { lat: 39.7896, lng: 38.7896 } },
+  { name: "Vet2", location: { lat: 39.2, lng: 39.0 } },
+];
 
 function MapComponent() {
   const [map, setMap] = useState(null);
@@ -16,14 +22,14 @@ function MapComponent() {
   useEffect(() => {
     const loader = new Loader({
       apiKey: API_KEY,
-      version: "weekly"
+      version: "weekly",
     });
 
     loader.load().then(() => {
       const { google } = window;
       const mapInstance = new google.maps.Map(document.getElementById("map"), {
         center: defaultCenter,
-        zoom: defaultZoom
+        zoom: defaultZoom,
       });
       setMap(mapInstance);
     });
@@ -31,8 +37,7 @@ function MapComponent() {
 
   return (
     <div>
-      <div id="map" style={{width:"100%", height:"100vh"}}></div> 
-      
+      <div id="map" style={{ width: "100%", height: "100vh" }}></div>
     </div>
   );
 }
