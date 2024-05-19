5992b577a50146f69de55d10c5feba0f10358649
İlayda Demirel <ilaydaddmrl@gmail.com>
2024-05-19 18:37:29 +0300

    Homepage ve navbar düzenlemesi

--- pawologue/src/Homepage/homepage.js
+++ pawologue/src/Homepage/homepage.js
@@ -1,55 +1,159 @@
-import React, { useState, useEffect } from "react";
-import dogPhoto from "../dog.jpg";
-import catPhoto from "../cat.jpg";
+import React from "react";
+import dogPhoto from "../dog.png";
+import catPhoto from "../cat.png";
 import Navbar from "../Navbar";
+import logobg from "../logobg.png"; 
 
 function HomePage() {
-
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-      }}
-    >
+    <div style={{
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      width: "100%",
+      position: "relative", 
+      minHeight: "100vh", 
+    }}>
       <Navbar />
-      <h1>Welcome to Pawologue</h1>
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <div style={{ margin: "20px", alignItems: "center" }}>
-          <p>
-            Dogs are domesticated mammals that belong to the Canidae family,
-            which also includes wolves, foxes, and other similar species. They
-            are highly social animals and often form strong bonds with their
-            human companions, earning them the title of "man's best friend."
-            Dogs have been selectively bred for thousands of years for various
-            purposes, including hunting, herding, guarding, and companionship,
-            resulting in the wide variety of breeds we see today.
-          </p>
+      <h1 style={{ marginTop: "20px" }}>Welcome to Pawologue!</h1>
+      <div style={{
+        display: "flex",
+        width: "100%",
+        justifyContent: "space-around",
+        flexWrap: "wrap"
+      }}>
+        <div style={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          margin: "20px",
+          marginTop:"20px",
+          marginLeft:"100px",
+          backgroundColor: "#eeebf0",
+          borderRadius: "20px",
+          padding: "20px",
+          width: "600px", 
+          height: "350px"
+        }}>
           <img
             src={dogPhoto}
-            style={{ width: "600px", marginLeft: "100px" }}
+            style={{ width: "300px", marginRight: "20px", height: "300px" }}
             alt="Dog"
           />
+          <div style={{ maxWidth: "300px" }}>
+            <h3>🐶 Dogs</h3>
+            <p>
+              Dogs are domesticated mammals that belong to the Canidae family,
+              which also includes wolves, foxes, and other similar species. They
+              are highly social animals and often form strong bonds with their
+              human companions, earning them the title of "man's best friend."
+              Dogs have been selectively bred for thousands of years for various
+              purposes, including hunting, herding, guarding, and companionship,
+              resulting in the wide variety of breeds we see today.
+            </p>
+          </div>
         </div>
-        <div style={{ margin: "20px", alignItems: "center" }}>
-          <p>
-            Cats are small carnivorous mammals known for their agility, grace,
-            and independent nature. They belong to the Felidae family, which
-            also includes lions, tigers, and other wild cats. Domestic cats are
-            descended from wild ancestors that were domesticated over 10,000
-            years ago, likely for their ability to control pests such as
-            rodents. Cats are highly territorial animals and often establish
-            territories that they defend from other cats.
-          </p>
+        <div style={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          margin: "20px",
+          marginTop: "300px",
+          marginRight: "100px",
+          backgroundColor: "#eeebf0",
+          borderRadius: "20px",
+          padding: "20px",
+          width: "600px", 
+          height: "350px"
+        }}>
+          <div style={{ maxWidth: "300px", marginRight: "20px" }}>
+            <h3>🐱 Cats</h3>
+            <p>
+              Cats are small carnivorous mammals known for their agility, grace,
+              and independent nature. They belong to the Felidae family, which
+              also includes lions, tigers, and other wild cats. Domestic cats are
+              descended from wild ancestors that were domesticated over 10,000
+              years ago, likely for their ability to control pests such as
+              rodents. Cats are highly territorial animals and often establish
+              territories that they defend from other cats.
+            </p>
+          </div>
           <img
             src={catPhoto}
-            style={{ width: "500px", marginLeft: "200px" }}
+            style={{ width: "300px", height: "300px" }}
             alt="Cat"
           />
         </div>
       </div>
+      
+      <img 
+        src={logobg} 
+        style={{
+          position: "absolute",
+          bottom: "20px",
+          left: "20px",
+          width: "100px",
+          transform: "rotate(15deg)"
+        }}
+        alt="Background Logo"
+      />
+      <img 
+        src={logobg} 
+        style={{
+          position: "absolute",
+          bottom: "10px",
+          left: "130px",
+          width: "150px",
+          transform: "rotate(-10deg)"
+        }}
+        alt="Background Logo"
+      />
+      <img 
+        src={logobg} 
+        style={{
+          position: "absolute",
+          bottom: "70px",
+          left: "90px",
+          width: "80px",
+          transform: "rotate(5deg)"
+        }}
+        alt="Background Logo"
+      />
+      <img 
+        src={logobg} 
+        style={{
+          position: "absolute",
+          bottom: "130px",
+          left: "60px",
+          width: "80px",
+          transform: "rotate(0deg)"
+        }}
+        alt="Background Logo"
+      />
+      <img 
+        src={logobg} 
+        style={{
+          position: "absolute",
+          bottom: "200px",
+          left: "20px",
+          width: "50px",
+          transform: "rotate(0deg)"
+        }}
+        alt="Background Logo"
+      />
+      <img 
+        src={logobg} 
+        style={{
+          position: "absolute",
+          bottom: "10px",
+          left: "250px",
+          width: "100px",
+          transform: "rotate(0deg)"
+        }}
+        alt="Background Logo"
+      />
     </div>
   );
 }
+
 export default HomePage;
--- pawologue/src/Navbar.js
+++ pawologue/src/Navbar.js
@@ -1,34 +1,64 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { getAuth, signOut } from "firebase/auth";
+import logo from "./logo.png";
 
 const styles = {
   container: {
     display: "flex",
     alignItems: "center",
     justifyContent: "space-between",
-    backgroundColor: "#6a1b9a",
+    backgroundColor: "#684589",
     color: "#fff",
     padding: "10px 20px",
     width: "100%",
     boxSizing: "border-box",
     boxShadow: "0 2px 4px rgba(0,0,0,0.1)",
   },
+  logo: {
+    height: "50px", 
+    marginRight: "10px"
+  },
   navbarLink: {
     textDecoration: "none",
     color: "#fff",
-    fontSize: "1.2rem",
+    fontSize: "1.5rem",
     fontWeight: "bold",
+    display: "flex",
+    alignItems: "center"
+  },
+  mainLinks: {
+    display: 'flex',
+    justifyContent: 'center',
+    flex: '1',
   },
   button: {
-    padding: "8px 16px",
+    padding: "8px 12px",
     borderRadius: "5px",
-    backgroundColor: "#fff",
-    color: "#6a1b9a",
+    backgroundColor: "#684589",
+    color: "#fff",
     textDecoration: "none",
     border: "none",
     cursor: "pointer",
+    textAlign: "center",
+    margin: "0 2px",
   },
+  authButtons: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
+  },
+  authButton: {
+    backgroundColor: "#fff",
+    color: "#6a1b9a",
+    padding: "8px 12px",
+    borderRadius: "5px",
+    textDecoration: "none",
+    border: "none",
+    cursor: "pointer",
+    textAlign: "center",
+    margin: "0 2px",
+  }
 };
 
 function Navbar() {
@@ -38,15 +68,11 @@ function Navbar() {
   const handleLogout = () => {
     signOut(auth)
       .then(() => {
-        // Clear currentUser from local storage
         localStorage.removeItem('currentUser');
-        // Başarıyla çıkış yapıldığında yapılacak işlemler
         console.log("User logged out successfully!");
-        // Kullanıcıyı "/" yoluna yönlendir
         navigate("/");
       })
       .catch((error) => {
-        // Çıkış sırasında hata oluştuğunda yapılacak işlemler
         console.error("Error signing out:", error);
       });
   };
@@ -54,45 +80,30 @@ function Navbar() {
   return (
     <div style={styles.container}>
       <Link to="/" style={styles.navbarLink}>
+        <img src={logo} alt="Logo" style={styles.logo}/> 
         <h3>Pawologue</h3>
       </Link>
-      <Link to="/adopt" style={styles.button}>
-        Adopt a Pet
-      </Link>
-      <Link to="/rehome" style={styles.button}>
-        Find a Home
-      </Link>
-      <Link to="/pairing" style={styles.button}>
-        Find a Partner
-      </Link>
-      <Link to="/dognotices" style={styles.button}>
-        Pet Sitting
-      </Link>
-      <Link to="/mapcomponent" style={styles.button}>
-        Map Component
-      </Link>
-      {auth.currentUser ? (
-        <>
-        <Link to="/chat" style={styles.button}>
-            Chat
-          </Link>
-          <Link to="/profile" style={styles.button}>
-            Profile
-          </Link>
-          <Link to="/" onClick={() => handleLogout()} style={styles.button}>
-            Log Out
-          </Link>
-        </>
-      ) : (
-        <div>
-          <Link to="/login" style={styles.button}>
-            Login
-          </Link>
-          <Link to="/signup" style={{ ...styles.button, marginLeft: "10px" }}>
-            Sign Up
-          </Link>
-        </div>
-      )}
+      <div style={styles.mainLinks}> 
+        <Link to="/adopt" style={styles.button}>Adopt a Pet</Link>
+        <Link to="/rehome" style={styles.button}>Find a Home</Link>
+        <Link to="/pairing" style={styles.button}>Find a Partner</Link>
+        <Link to="/dognotices" style={styles.button}>Find a Pet Sitter</Link>
+        <Link to="/mapcomponent" style={styles.button}>View Map</Link>
+      </div>
+      <div style={styles.authButtons}>
+        {auth.currentUser ? (
+          <>
+            <Link to="/chat" style={styles.button}>Chat</Link>
+            <Link to="/profile" style={styles.button}>Profile</Link>
+            <Link to="/" onClick={handleLogout} style={styles.authButton}>Log Out</Link>
+          </>
+        ) : (
+          <>
+            <Link to="/login" style={styles.authButton}>Login</Link>
+            <Link to="/signup" style={styles.authButton}>Sign Up</Link>
+          </>
+        )}
+      </div>
     </div>
   );
 }
